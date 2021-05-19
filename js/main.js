95f4b76c12a6988fbbf00237e68cc7c4c5c55970
zile028 <zile028@gmail.com>
2021-05-19 18:35:59 +0200

    add navbar function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 const navbar = document.getElementById("navbar");
 
 window.addEventListener("scroll", function () {
-  console.log(window.scrollY);
-  if (window.scrollY > 0) {
-    navbar.style.top = 0;
-  } else if (window.scrollY == 0) {
-    navbar.style.top = 30;
+  let posY = window.scrollY;
+  // console.log(posY);
+  if (posY < 30) {
+    navbar.style.top = "30px";
+    navbar.style.width = "90%";
+  } else {
+    navbar.style.top = "0px";
+    navbar.style.width = "100%";
   }
 });
